ce07998dc4fb7cd43798e8fc5f8e96acf4b5c357
Edward <edwardfernande26@gmail.com>
2016-04-05 18:14:49 -0430

    Estado inicial del proyecto.

---
+++ Assets/scripts/resolutionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resolutionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// We set the 64x64 resolution with a window windowed
+		Screen.SetResolution (64, 64, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
